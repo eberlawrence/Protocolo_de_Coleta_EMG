f35d05df4db51c7ce9f26514897686977f02f6f6
Eber Lawrence <eberlawrence@hotmail.com>
2018-10-02 15:09:01 -0300

    First commit.

---
+++ ProtocoloExperimental/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ProtocoloExperimental
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.TimerOne = new System.Windows.Forms.Timer(this.components);
+            this.lbCountT = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lbCountP = new System.Windows.Forms.Label();
+            this.lbStartMessage = new System.Windows.Forms.Label();
+            this.TimerTwo = new System.Windows.Forms.Timer(this.components);
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.lbCount = new System.Windows.Forms.Label();
+            this.pbTrigger = new System.Windows.Forms.PictureBox();
+            this.lb3 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.btOne = new System.Windows.Forms.Button();
+            this.pbTwo = new System.Windows.Forms.PictureBox();
+            this.lbMoviment = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.pnMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTrigger)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTwo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbMain.BackColor = System.Drawing.Color.White;
+            this.pbMain.Location = new System.Drawing.Point(365, 12);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(349, 502);
+            this.pbMain.TabIndex = 1;
+            this.pbMain.TabStop = false;
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btStart.Location = new System.Drawing.Point(99, 64);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(178, 70);
+            this.btStart.TabIndex = 3;
+            this.btStart.Text = "Iniciar a coleta";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.Bt1_Click);
+            // 
+            // TimerOne
+            // 
+            this.TimerOne.Interval = 1000;
+            this.TimerOne.Tick += new System.EventHandler(this.TimerOne_Tick);
+            // 
+            // lbCountT
+            // 
+            this.lbCountT.AutoSize = true;
+            this.lbCountT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCountT.Location = new System.Drawing.Point(293, 224);
+            this.lbCountT.Name = "lbCountT";
+            this.lbCountT.Size = new System.Drawing.Size(23, 25);
+            this.lbCountT.TabIndex = 4;
+            this.lbCountT.Text = "0";
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(22, 224);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(265, 25);
+            this.lb2.TabIndex = 5;
+            this.lb2.Text = "Número total de movimentos:";
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(22, 189);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(298, 25);
+            this.lb1.TabIndex = 6;
+            this.lb1.Text = "Número de movimentos da série:";
+            // 
+            // lbCountP
+            // 
+            this.lbCountP.AutoSize = true;
+            this.lbCountP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCountP.Location = new System.Drawing.Point(326, 189);
+            this.lbCountP.Name = "lbCountP";
+            this.lbCountP.Size = new System.Drawing.Size(23, 25);
+            this.lbCountP.TabIndex = 7;
+            this.lbCountP.Text = "0";
+            // 
+            // lbStartMessage
+            // 
+            this.lbStartMessage.AutoSize = true;
+            this.lbStartMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStartMessage.Location = new System.Drawing.Point(12, 12);
+            this.lbStartMessage.Name = "lbStartMessage";
+            this.lbStartMessage.Size = new System.Drawing.Size(0, 46);
+            this.lbStartMessage.TabIndex = 8;
+            // 
+            // TimerTwo
+            // 
+            this.TimerTwo.Interval = 1000;
+            this.TimerTwo.Tick += new System.EventHandler(this.TimerTwo_Tick);
+            // 
+            // pnMain
+            // 
+            this.pnMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnMain.Controls.Add(this.lbMoviment);
+            this.pnMain.Controls.Add(this.lbCount);
+            this.pnMain.Controls.Add(this.pbTrigger);
+            this.pnMain.Controls.Add(this.lb3);
+            this.pnMain.Controls.Add(this.tbName);
+            this.pnMain.Controls.Add(this.lbCountP);
+            this.pnMain.Controls.Add(this.lbStartMessage);
+            this.pnMain.Controls.Add(this.lb1);
+            this.pnMain.Controls.Add(this.btOne);
+            this.pnMain.Controls.Add(this.lb2);
+            this.pnMain.Controls.Add(this.btStart);
+            this.pnMain.Controls.Add(this.lbCountT);
+            this.pnMain.Controls.Add(this.pbTwo);
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(731, 526);
+            this.pnMain.TabIndex = 9;
+            this.pnMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnMain_Paint);
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCount.Location = new System.Drawing.Point(140, 273);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(110, 120);
+            this.lbCount.TabIndex = 13;
+            this.lbCount.Text = "0";
+            // 
+            // pbTrigger
+            // 
+            this.pbTrigger.Location = new System.Drawing.Point(36, 14);
+            this.pbTrigger.Name = "pbTrigger";
+            this.pbTrigger.Size = new System.Drawing.Size(8, 8);
+            this.pbTrigger.TabIndex = 12;
+            this.pbTrigger.TabStop = false;
+            // 
+            // lb3
+            // 
+            this.lb3.AutoSize = true;
+            this.lb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb3.Location = new System.Drawing.Point(22, 156);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(186, 25);
+            this.lb3.TabIndex = 11;
+            this.lb3.Text = "Nome do voluntário:";
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbName.Location = new System.Drawing.Point(214, 153);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(135, 30);
+            this.tbName.TabIndex = 10;
+            // 
+            // btOne
+            // 
+            this.btOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btOne.Location = new System.Drawing.Point(4, 48);
+            this.btOne.Name = "btOne";
+            this.btOne.Size = new System.Drawing.Size(10, 10);
+            this.btOne.TabIndex = 0;
+            this.btOne.Text = "Pronto";
+            this.btOne.UseVisualStyleBackColor = true;
+            this.btOne.Click += new System.EventHandler(this.btOne_Click);
+            // 
+            // pbTwo
+            // 
+            this.pbTwo.Location = new System.Drawing.Point(4, 12);
+            this.pbTwo.Name = "pbTwo";
+            this.pbTwo.Size = new System.Drawing.Size(10, 10);
+            this.pbTwo.TabIndex = 9;
+            this.pbTwo.TabStop = false;
+            // 
+            // lbMoviment
+            // 
+            this.lbMoviment.AutoSize = true;
+            this.lbMoviment.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMoviment.Location = new System.Drawing.Point(16, 318);
+            this.lbMoviment.Name = "lbMoviment";
+            this.lbMoviment.Size = new System.Drawing.Size(0, 46);
+            this.lbMoviment.TabIndex = 14;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(731, 526);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.pnMain);
+            this.Name = "MainForm";
+            this.Text = "Protocolo Experiental";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.pnMain.ResumeLayout(false);
+            this.pnMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTrigger)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTwo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Timer TimerOne;
+        private System.Windows.Forms.Label lbCountT;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lbCountP;
+        private System.Windows.Forms.Label lbStartMessage;
+        private System.Windows.Forms.Timer TimerTwo;
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Button btOne;
+        private System.Windows.Forms.PictureBox pbTwo;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lb3;
+        private System.Windows.Forms.PictureBox pbTrigger;
+        private System.Windows.Forms.Label lbCount;
+        private System.Windows.Forms.Label lbMoviment;
+    }
+}
+
---
+++ ProtocoloExperimental/Form1.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProtocoloExperimental
+{
+    public partial class MainForm : Form
+    {
+
+
+        Random randonNum = new Random();
+        List<Bitmap> imageList = new List<Bitmap>();
+        List<int> fourMoviments = new List<int>() { 1, 2, 3, 4 };
+        int[] dataList = new int[360];
+        Dictionary<int, List<int>> data = new Dictionary<int, List<int>>();
+        Bitmap img1 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\Neutro.jpg");
+        Bitmap img2 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\NeutroSupina.jpg");
+        Bitmap img3 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\NeutroProna.jpg");
+        Bitmap img4 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\NeutroAbre.jpg");
+        Bitmap img5 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\NeutroFecha.jpg");
+        Bitmap img6 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\Supina.jpg");
+        Bitmap img7 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\Prona.jpg");
+        Bitmap img8 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\Abre.jpg");
+        Bitmap img9 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\Fecha.jpg");
+        Bitmap img10 = new Bitmap(@"D:\Arquivos\Graduaçao Eng. Biomedica\10º Periodo\Trabalho de Conclusão de Curso - TCC\Imagens\Final\Modelo.jpg");
+
+        int flag = 0;
+
+        int x; int y;
+
+        int i = 0; int j = 0;
+
+        int a = 1; int b = 5; int c = 4;
+
+        bool iTwo = false;        bool iThree = false;        bool iFour = false;
+
+        int countOne = 0;        int countTwo = 0;        int countThree = 0;        int countFour = 0;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            TimerTwo.Start();
+
+            btOne.Visible = false;
+            btStart.Visible = false;
+
+            tbName.Visible = false;
+
+            lb1.Visible = false;
+            lb2.Visible = false;
+            lb3.Visible = false;
+            lbCount.Visible = false;
+            lbCountP.Visible = false;
+            lbCountT.Visible = false;
+            lbMoviment.Visible = false;
+            lbStartMessage.Visible = false;
+
+            pbMain.Visible = false;
+            pbTwo.Visible = false;
+
+        }
+
+
+
+        private void pnMain_Paint(object sender, PaintEventArgs e)
+        {
+            x = (pnMain.Size.Width) / 2;
+            y = (pnMain.Size.Height - pbMain.Height) / 2;
+            pbMain.Location = new Point(x, y);
+
+
+            x = (pnMain.Size.Width + pbMain.Width - (lbCount.Width)/2 ) / 2;
+            y = (pnMain.Size.Height - lbCount.Height);
+            lbMoviment.Location = new Point(x, y);
+
+
+            x = (pnMain.Size.Width + pbMain.Width - (lbCount.Width/2)) / 2;
+            y = (lbCount.Size.Height) / 2;
+            lbCount.Location = new Point(x, y);
+        }
+
+
+        private void TimerTwo_Tick(object sender, EventArgs e)
+        {
+            lbStartMessage.Visible = true;
+            if (flag == 0)
+            {
+
+                lbStartMessage.Text = "Bem vindo ao experimento!!";
+                x = (pnMain.Size.Width - lbStartMessage.Width) / 2;
+                y = (pnMain.Size.Height - lbStartMessage.Height) / 2;
+                lbStartMessage.Location = new Point(x, y);
+            }
+            if (flag == 1)
+            {
+                lbStartMessage.Text = "O experimento consiste em coletar sinais EMG de quatro canais, \nsendo cada um com um par de eletrodos ativos equidistantes em \ntorno do antebraço do voluntário com auxílio de um bracelete.";
+                x = (pnMain.Size.Width - lbStartMessage.Width) / 2;
+                y = (pnMain.Size.Height - lbStartMessage.Height) / 2;
+                lbStartMessage.Location = new Point(x, y);
+            }
+            if (flag == 2)
+            {
+                pbTwo.Visible = true;
+                pbTwo.Width = 191;
+                pbTwo.Height = 429;
+                x = (pnMain.Size.Width - pbTwo.Width) / 2;
+                y = (pnMain.Size.Height - pbTwo.Height) / 2;
+                pbTwo.Location = new Point(x, y);
+
+                pbTwo.Image = img10;
+                lbStartMessage.Visible = false;
+                btOne.Visible = true;
+                btOne.Width = 180;
+                btOne.Height = 45;
+                x = (pnMain.Size.Width - btOne.Width) / 2;
+                y = (pnMain.Size.Height + pbTwo.Height) / 2;
+                btOne.Location = new Point(x, y);
+                TimerTwo.Stop();
+            }
+            flag++;
+        }
+
+
+        private void btOne_Click(object sender, EventArgs e)
+        {
+            btOne.Visible = false;
+            btStart.Visible = true;
+
+            tbName.Visible = true;
+
+            lb1.Visible = true;
+            lb2.Visible = true;
+            lb3.Visible = true;
+            lbCount.Visible = true;
+            lbCountP.Visible = true;
+            lbCountT.Visible = true;
+            lbMoviment.Visible = true;
+
+            pbMain.Visible = true;
+            pbTwo.Visible = false;
+        }
+
+
+        private void ImageShow()
+        {                  
+            imageList.Add(img1);
+            imageList.Add(img2);
+            imageList.Add(img3);
+            imageList.Add(img4);
+            imageList.Add(img5);
+            imageList.Add(img6);
+            imageList.Add(img7);
+            imageList.Add(img8);
+            imageList.Add(img9);
+
+            for (int i = 0; i < 10; i++)
+            {
+                data.Add(i, fourMoviments.OrderBy(s => randonNum.Next()).Take(4).ToList());                
+            }        
+        }
+
+        
+        private void Bt1_Click(object sender, EventArgs e)
+        {
+            for (int i = data.Count; i >= 0; i--)
+            {
+                data.Remove(i);
+            }
+
+            if (btStart.Text == "Iniciar a coleta")
+            {
+                btStart.Text = "Parar a coleta";
+                ImageShow();
+                TimerOne.Start();
+            }
+            else
+            {
+                btStart.Text = "Iniciar a coleta";
+                TimerOne.Stop();
+                iTwo = false;
+                iThree = false;
+                iFour = false;
+                j = 0;
+                i = 0;
+                c = 3;
+                countOne = 0;
+                countTwo = 0;
+                countThree = 0;
+                countFour = 0; 
+                lbCountT.Text = "0";
+                lbCountP.Text = "0";
+            }         
+        }
+
+
+        private void DataList()
+        {
+            if (pbMain.Image == imageList[0])
+                dataList[i] = 0;
+
+            if (pbMain.Image == imageList[1])
+                dataList[i] = 1;
+
+            if (pbMain.Image == imageList[2])            
+                dataList[i] = 2;
+            
+            if (pbMain.Image == imageList[3])
+                dataList[i] = 3;
+
+            if (pbMain.Image == imageList[4])
+                dataList[i] = 4;
+
+            if (pbMain.Image == imageList[5])
+                dataList[i] = 5;
+
+            if (pbMain.Image == imageList[6])
+                dataList[i] = 6;
+
+            if (pbMain.Image == imageList[7])
+                dataList[i] = 7;
+
+            if (pbMain.Image == imageList[8])
+                dataList[i] = 8;
+
+            i++;
+        }
+
+        private void MovimentText(Image x)
+        {
+            if (x == img2)
+            {
+                lbMoviment.Text = "Tempo em repouso";
+            }
+
+        }
+        private void TimerOne_Tick(object sender, EventArgs e)
+        {
+
+            if (j < 10)
+            {
+                if (countTwo < 4)
+                {
+                    if (countOne < 3)
+                    {
+                        c--;
+                        lbCount.Text = c.ToString();
+                        pbMain.Image = imageList[0];
+                        lbMoviment.Text = "Tempo em repouso";
+                    }
+                    if (countOne >= 3 && countOne < 6)
+                    {
+                        TimeMovement(3);
+
+                        if (pbMain.Image == imageList[0] && iTwo == false
+                               && iThree == false && iFour == false)
+                        {
+                            pbMain.Image = imageList[data[j][0]];
+                        }
+                        if (iTwo == true)
+                        {
+                            pbMain.Image = imageList[data[j][1]];
+                            iTwo = false;
+                        }
+                        if (iThree == true)
+                        {
+                            pbMain.Image = imageList[data[j][2]];
+                            iThree = false;
+                        }
+                        if (iFour == true)
+                        {
+                            pbMain.Image = imageList[data[j][3]];
+                            iFour = false;
+                        }
+                    }
+                    if (countOne >= 6 && countOne < 8)
+                    {
+                        TimeMovement(6);
+
+                        if (pbMain.Image == imageList[data[j][0]])
+                        {
+                            iTwo = true;
+                            pbMain.Image = imageList[data[j][0] + 4];
+                        }
+
+                        if (pbMain.Image == imageList[data[j][1]])
+                        {
+                            iThree = true;
+                            pbMain.Image = imageList[data[j][1] + 4];
+                        }
+                        if (pbMain.Image == imageList[data[j][2]])
+                        {
+                            iFour = true;
+                            pbMain.Image = imageList[data[j][2] + 4];
+                        }
+
+                        if (pbMain.Image == imageList[data[j][3]])
+                        {
+                            pbMain.Image = imageList[data[j][3] + 4];
+                        }
+                    }
+                    if (countOne == 8)
+                    {
+                        lbCount.Text = c.ToString();
+                        c = 4;
+                        countThree++;
+                        countFour++;
+                        lbCountT.Text = countThree.ToString();
+                        lbCountP.Text = countFour.ToString();
+                        countOne = -1;
+                        countTwo++;
+                    }
+
+                    
+                    countOne++;
+                    DataList();
+                }
+                else
+                {
+                    j++;
+                    countTwo = 0;
+                }
+            }
+            else
+            {
+                i = 0;
+                j = 0;
+                countFour = 0;
+                TimerOne.Stop();
+                lbCountP.Text = "0";
+                btStart.Text = "Iniciar a coleta";
+
+                DataSave();
+                a++;
+                b++;
+            }
+
+
+        }
+
+        private void TimeMovement(int x)
+        {
+            if (countOne == x)
+            {
+                c = 3;
+            }
+            lbCount.Text = c.ToString();
+            c--;
+        }
+        private void DataSave()
+        {
+            var fullPathOne = @"C:\" + tbName.Text + "-Coleta-" + a.ToString() + ".txt";
+            using (var write = new StreamWriter(fullPathOne))
+            {
+                foreach (var n in dataList)
+                    write.WriteLine(n);
+
+            }
+            var fullPathTwo = @"C:\" + tbName.Text + "-Coleta-" + b.ToString() + ".txt";
+            using (var write = new StreamWriter(fullPathTwo))
+            {
+                foreach (var n in dataList)
+                    if (n != dataList[0])
+                    {
+                        write.WriteLine(n);
+                    }
+            }
+        }
+    }
+}
